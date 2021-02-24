29cceba39a19bc367dc0a7cc87ee46f5ceb9cdc0
unknown <asmaakhaled603@gmail.com>
2021-02-24 20:26:29 +0200

    Add Tasks of Day01 with Bonus Task

---
+++ Day01/Bonus-Task/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bonus_Task
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Buttons Clicked Function
+       
+        private void setText(object sender, RoutedEventArgs e)
+        {
+            
+            txt1.Text = "Hello Every One from Set";
+            
+
+
+        }
+        
+        private void selectAll(object sender, RoutedEventArgs e)
+        {
+            txt1.SelectAll();txt1.Focusable = true;
+        }
+        private void clearText(object sender, RoutedEventArgs e)
+        {
+          
+            txt1.Clear();
+        }
+        private void prependText(object sender, RoutedEventArgs e)
+        {
+            string prependValue = "***Prepend***";
+            string newText;
+            //txt1.PrependText("gg");
+            newText = prependValue + txt1.Text;
+            txt1.Text = newText;
+        }
+        private void insetText(object sender, RoutedEventArgs e)
+        {
+            string oldValue = txt1.Text;
+            txt1.Text  =oldValue +  " Hello Every One from insert";
+        }
+        private void appendText(object sender, RoutedEventArgs e)
+        {
+           
+            txt1.AppendText("***append***");
+        }
+        private void cutText(object sender, RoutedEventArgs e)
+        {
+            
+            if (txt1.SelectedText != "")
+                txt1.Cut();
+        }
+        private void pastText(object sender, RoutedEventArgs e)
+        {
+            txt1.Paste();
+            
+        }
+        private void undoText(object sender, RoutedEventArgs e)
+        {
+            txt1.Undo();
+           
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rBtn1= (RadioButton)sender;
+            string contentValue = rBtn1.Content.ToString();
+            switch (contentValue)
+            {
+                case "Editable":
+                    txt1.IsReadOnly = false;
+                    break;
+                case "Read Only":
+                    txt1.IsReadOnly = true;
+                    
+                    break;
+            }
+        }
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            RadioButton rBtn1 = (RadioButton)sender;
+            string contentValue = rBtn1.Content.ToString();
+            switch (contentValue)
+            {
+                case "Left":
+                    txt1.TextAlignment = TextAlignment.Left;
+                    break;
+                case "Center":
+                    txt1.TextAlignment = TextAlignment.Center;
+                    break;
+                case "Right":
+                    txt1.TextAlignment = TextAlignment.Right;
+                    break;
+            }
+        }
+    }
+}
---
+++ Day01/Lab1-Task2/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab1_Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            switch (((RadioButton)sender).Content.ToString())
+            {
+                case "Red":
+                    ink.DefaultDrawingAttributes.Color = Colors.Red;
+                    break;
+                case "Green":
+                    ink.DefaultDrawingAttributes.Color = Colors.Green;
+                    break;
+                case "Blue":
+                    ink.DefaultDrawingAttributes.Color = Colors.Blue;
+                    break;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            switch (((RadioButton)sender).Content.ToString())
+            {
+                case "Ink":
+                    ink.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Select":
+                    ink.EditingMode = InkCanvasEditingMode.Select;
+
+                    break;
+                case "Erase":
+                    ink.EditingMode = InkCanvasEditingMode.EraseByPoint;
+
+                    break;
+            }
+
+        }
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            switch (((RadioButton)sender).Content.ToString())
+            {
+                case "Elipse":
+                 ink.DefaultDrawingAttributes.StylusTip = StylusTip.Ellipse;
+                    break;
+                case "Rect":
+                    ink.DefaultDrawingAttributes.StylusTip = StylusTip.Rectangle;
+
+                    break;
+            }
+
+        }
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            switch (((RadioButton)sender).Content.ToString())
+            {
+                case "small":
+                    ink.DefaultDrawingAttributes.Height = 10;
+                    ink.DefaultDrawingAttributes.Width = 10;
+                    break;
+                case "normal":
+                    
+                    ink.DefaultDrawingAttributes.Height = 30;
+                    ink.DefaultDrawingAttributes.Width =30;
+
+                    break;
+                case "large":
+                    ink.DefaultDrawingAttributes.Height = 50;
+                    ink.DefaultDrawingAttributes.Width = 40;
+                    break;
+            }
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ink.Strokes.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ink.CopySelection();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ink.CutSelection();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ink.Paste();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            FileStream f = new FileStream("D:/WPF/Day01/Lab1-Task2/canvas.ink", FileMode.Create, FileAccess.Write);
+            ink.Strokes.Save(f);
+            f.Close();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+            FileStream r = new FileStream("D:/WPF/Day01/Lab1-Task2/canvas.ink", FileMode.Open, FileAccess.Read);
+            StrokeCollection strokes = new StrokeCollection(r);
+            ink.Strokes = strokes;
+            r.Close();
+        }
+    }
+}
