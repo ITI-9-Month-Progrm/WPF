1fea90d73bf3f664c9148815f650108ea30d6e2d
unknown <asmaakhaled603@gmail.com>
2021-03-06 00:28:52 +0200

    Add Final Taslk 'mickey'

--- Day02/Lab02-Task02/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Day02/Lab02-Task02/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace Lab02_Task02 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Lab02-Task02;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Lab02-Task02;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Day03/Lab03-Task03/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Day03/Lab03-Task03/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace Lab03_Task03 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Lab03-Task03;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Lab03-Task03;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
